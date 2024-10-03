61d9a27711b02f7d07e89903fdf0b55d3637ce71
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-10-03 08:01:43 +0700

    mengerjakan peranyaan 1

--- Pemilihan2Percobaan103.java
+++ Pemilihan2Percobaan103.java
@@ -1,19 +1,19 @@
-
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan103{
     public static void main(String[] args){
         Scanner input03 = new Scanner(System.in);
 
-        System.err.print("Masukkan tahun : ");
+        System.out.print("Masukkan tahun : ");
         int thn =input03.nextInt();
 
         if ((thn % 4 ) == 0){
             if ((thn%100)!=0){
                 System.out.println("Tahun Kabisat");
-            }
+            }else
+            System.out.println("Tahun "+thn+" bukan tahun kabisat");
         }else{
-            System.err.println("Bukan tahun kabisat");
+            System.out.println("Bukan tahun kabisat");
         }
     }
 }
