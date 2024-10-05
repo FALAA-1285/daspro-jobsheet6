37af59fb25458ef4f581866a02bdcee2bc2dd88c
FALAA-1285 <ahmadfalahi1285@gmail.com>
2024-10-05 11:12:40 +0700

    percobaan3

---
+++ Pemilihan2Percobaan303.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan303 {
+    public static void main(String[] args) {
+        Scanner input03=new Scanner(System.in);
+
+        String ketagori;
+        int gajiBersih, penghasilan;
+        double pajak=0;
+
+        System.out.print("Masukkan ketagori : ");
+        ketagori=input03.nextLine();
+        System.out.print("Masukkan besarnya penghasilan : ");
+        penghasilan=input03.nextInt();
+
+        if(ketagori.equalsIgnoreCase("pekerja")){
+            if(penghasilan<=2000000){
+                pajak=0.1;
+            }else if(penghasilan<=3000000){
+                pajak=0.15;
+            }else{
+                pajak=0.2;
+            }
+            gajiBersih=(int)(penghasilan-(penghasilan*pajak));
+            System.out.println("penghasilan bersih : "+gajiBersih);
+        }else if (ketagori.equalsIgnoreCase("pebisnis")){
+            if(penghasilan<=2500000){
+                pajak=0.15;
+            }else if (penghasilan<=3500000){
+                pajak=0.2;
+            }else{
+                pajak=0.25;
+            }
+            gajiBersih=(int)(penghasilan-(penghasilan*pajak));
+            System.out.println("Penghasilan bersih : "+gajiBersih);
+        }else{
+            System.out.println("Masukkan ketagori salah");
+        }
+    }
+    
+}
